4a655911b0e5f5c3aea12bb89bb0b65c3dbf5072
canuludag <cucanuludag@gmail.com>
2017-12-14 16:39:53 +0300

    feat: Add Instrumentation test with Espresso Test Recorder

---
+++ app/src/androidTest/java/com/uludag/can/plantplacespacktfortdd/ui/SearchPlantsActivityTest2.java
@@ -0,0 +1,110 @@
+package com.uludag.can.plantplacespacktfortdd.ui;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.uludag.can.plantplacespacktfortdd.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SearchPlantsActivityTest2 {
+
+    @Rule
+    public ActivityTestRule<SearchPlantsActivity> mActivityTestRule = new ActivityTestRule<>(SearchPlantsActivity.class);
+
+    @Test
+    public void searchPlantsActivityTest2() {
+        ViewInteraction appCompatAutoCompleteTextView = onView(
+                allOf(withId(R.id.actPlantName),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatAutoCompleteTextView.perform(click());
+
+        ViewInteraction appCompatAutoCompleteTextView2 = onView(
+                allOf(withId(R.id.actPlantName),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatAutoCompleteTextView2.perform(replaceText("Redbud"), closeSoftKeyboard());
+
+        ViewInteraction appCompatAutoCompleteTextView3 = onView(
+                allOf(withId(R.id.actPlantName), withText("Redbud"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatAutoCompleteTextView3.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btnSearchPlants), withText("Search Plants"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.tv_plant_name), withText("Eastern Redbud"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.recyclerView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Eastern Redbud")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
